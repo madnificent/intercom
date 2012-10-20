1023c5bd8989043851857bcd03ea0b9a9be1a93b
Aad Versteden <madnificent@gmail.com>
2012-10-20 13:20:29 +0200

    exporting the functions for getting and setting variables in the screen and the session

--- lisp/packages.lisp
+++ lisp/packages.lisp
@@ -3,4 +3,6 @@
   (:use :cl :jsown :alexandria)
   (:export :define-remote-procedure
            :message :activep
-           :*remote-procedure-context*))
+           :*remote-procedure-context* :*hydra-head-timeout* :*hydra-body-timeout*
+           ;;---? should with-session-db-lock and with-screen-db-lock be exported too?
+           :screen-var :session-var))
